896ed2fe093f55af2efe2096552ae7b4dcbee469
llaJlbl4 <148627445+llaJlbl4@users.noreply.github.com>
2024-05-18 16:31:51 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,53 @@
+import face_recognition
 import cv2
-import numpy as np
+import os
 
+# Путь к папке с изображениями известных лиц
+known_faces_dir = 'faces'
 
-img = np.zeros((500, 500, 3), dtype='uint8')
+# Загрузка изображений известных лиц и кодирование их признаков
+known_face_encodings = []
+known_face_names = []
+for filename in os.listdir(known_faces_dir):
+    if filename.endswith('.jpg') or filename.endswith('.png'):
+        image = face_recognition.load_image_file(os.path.join(known_faces_dir, filename))
+        face_encoding = face_recognition.face_encodings(image)[0]
+        known_face_encodings.append(face_encoding)
+        known_face_names.append(os.path.splitext(filename)[0])
 
-# img [100:150, 200:280] = 0, 0, 255
-# img [450:500, 450:500] = 0, 255, 0
-cv2.rectangle(img, (250, 200), (300, 250), (255, 100, 50), thickness=1)
-cv2.rectangle(img, (200, 180), (270, 225), (0, 0, 255), thickness=cv2.FILLED)
-cv2.rectangle(img, (260, 230), (400, 190), (0, 255, 0), thickness=2)
-cv2.line(img, (250, 200), (400, 190), (255, 0, 0), thickness=1)
-cv2.circle(img, (280, 212), 150, (0, 0, 255), thickness=1)
-cv2.putText(img, "My text", (100, 200), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 0, 0), 2)
+# Включение веб-камеры
+cap = cv2.VideoCapture(0)
 
-cv2.imshow("IMG", img)
-cv2.waitKey(0)
+while True:
+    # Чтение кадра с веб-камеры
+    ret, frame = cap.read()
+
+    # Нахождение лиц на кадре
+    face_locations = face_recognition.face_locations(frame)
+    face_encodings = face_recognition.face_encodings(frame, face_locations)
+
+    # Сравнение каждого найденного лица с известными лицами
+    for face_encoding, face_location in zip(face_encodings, face_locations):
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        name = "Unknown"
+
+        # Если найдено совпадение, берется имя первого совпадения
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = known_face_names[first_match_index]
+
+        # Отрисовка рамки вокруг лица и отображение имени
+        top, right, bottom, left = face_location
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+        cv2.putText(frame, name, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+    # Отображение кадра с распознанными лицами
+    cv2.imshow('Face Recognition', frame)
+
+    # Выход из цикла по нажатию клавиши 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Освобождение ресурсов
+cap.release()
+cv2.destroyAllWindows()
